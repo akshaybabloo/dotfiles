7b540e9d865448db0a3f24a7b2e12b4729bb2797
Akshay Raj Gollahalli <akshay.gollahalli@fisherpaykel.com>
2023-09-13 14:02:55 +1200

    Use markdown syntax for extraction

--- docs/index.md
+++ docs/index.md
@@ -3,4 +3,4 @@ layout: doc
 title: Dotfiles
 ---
 
-
+<<< @/help.md
--- extract_man.sh
+++ extract_man.sh
@@ -7,8 +7,10 @@ process_file() {
     local file=$1
     local type=$2
 
+    # Add a heading to the man page content
     man_page_content+="## $type\n\n"
 
+    # Read the file line by line
     while IFS= read -r line; do
         # If the line starts with ##, it's a comment. Add it to the comments variable
         if [[ $line == \#\#* ]]; then
@@ -18,7 +20,7 @@ process_file() {
         # If the line starts with function or alias, it's a declaration. Extract the name and add it to the man page content
         elif [[ $line == function* ]] || [[ $line == alias* ]]; then
             name=$(echo $line | awk '{print $2}' | cut -d'=' -f1)
-            man_page_content+="$comments\n**$type:** $name\n\n"
+            man_page_content+="### $name\n\n$comments\n"
             # Reset comments
             comments=""
         fi
@@ -28,5 +30,4 @@ process_file() {
 process_file ".functions" "Functions"
 process_file ".aliases" "Aliases"
 
-# Print the man page content
-echo -e $man_page_content | less -R
+echo -e $man_page_content > $(pwd)/docs/help.md
