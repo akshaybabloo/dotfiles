adea05b900e98b6de5f3736f649949039c4c18d6
Akshay Raj Gollahalli <akshay@gollahalli.com>
2024-10-19 13:32:49 +1300

    Remove updater and move it binstall repo

--- updater.sh
+++ updater.sh
@@ -1,42 +1,71 @@
 #!/bin/bash
 
 current_dir=$(pwd)
-go_path=$HOME/go
+github_link="https://api.github.com/repos/akshaybabloo/binstall/releases/latest"
 
-printf "Checking if Go is installed...\n"
-
-function install_go() {
-    curl -o go.tar.gz "https://dl.google.com/go/$(curl https://go.dev/VERSION?m=text | head -n1).linux-amd64.tar.gz"
-    mkdir -p $go_path
-    tar -xvzf go.tar.gz -C $HOME
-    rm go.tar.gz
-    printf "\nexport PATH=\"\$PATH:$go_path/bin"\" >> $HOME/.bashrc
-    source $HOME/.bashrc
+function os_arch() {
+    os=$(uname -s | tr '[:upper:]' '[:lower:]')
+    arch=$(uname -m)
+    case $arch in
+    x86_64)
+        arch="amd64"
+        ;;
+    arm64)
+        arch="arm64"
+        ;;
+    esac
+    echo "$os $arch"
 }
 
-if ! command -v go &> /dev/null; then
-    printf "Go is not installed. Would you like to install it?\n"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) install_go; break;;
-            No ) break;;
-        esac
-    done
+function download_and_install_binstall() {
+    printf "Downloading latest binstall...\n"
+
+    # Create $HOME/bin if it doesn't exist
+    mkdir -p $HOME/bin
+
+    json=$(curl -s $github_link)
+
+    # Check arch and os in the json
+    os_arch=$(os_arch)
+
+    # split os_arch into os and arch
+    os=$(echo $os_arch | cut -d ' ' -f 1)
+    arch=$(echo $os_arch | cut -d ' ' -f 2)
+
+    download_url=$(echo $json | jq -r ".assets[] | select(.name | test(\"$os\") and test(\"$arch\")) | .browser_download_url")
+    # curl -sL $download_url | tar xz -C $HOME/bin --strip-components=1 binstall
+    curl -sL $download_url -o temp_archive.tar.gz
+    tar xzf temp_archive.tar.gz -C $HOME/bin binstall
+    rm temp_archive.tar.gz
+}
+
+function check_for_new_updater() {
+    version=$(binstall --version)
+    latest_version=$(curl -s $github_link | jq -r ".tag_name")
+
+    if [ "$version" != "$latest_version" ]; then
+        printf "New version of updater is available. Updating now...\n"
+        download_and_install_binstall
+    fi
+}
+
+printf "Checking if updater is installed...\n"
+
+# Check if "binstall" is installed
+if ! command -v binstall &>/dev/null; then
+    printf "binstall is not installed. Installing it now...\n"
+    # Check if "jq" is installed
+    if ! command -v jq &>/dev/null; then
+        printf "jq is not installed. Please install it first.\n"
+        exit 1
+    fi
+    download_and_install_binstall
 fi
 
-printf "Building updater...\n"
+# Check for new updates
+check_for_new_updater
 
-# Create $HOME/bin if it doesn't exist
-mkdir -p $HOME/bin
+printf "Updater installed. Checking for updates...\n"
 
-# Build dotfiles-updater and move to $HOME/bin
-cd $current_dir/updater
-$go_path/bin/go build -o dotfiles-updater main.go
-mv dotfiles-updater $HOME/bin/dotfiles-updater
-
-printf "Updater built successfully!\n\n"
-
-cd $current_dir
-
-# Initial run of updater
-$HOME/bin/dotfiles-updater download $current_dir/binary_configs/
+# Check for updates
+binstall download $current_dir/binary_configs
